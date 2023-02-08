a7f302eb238119fe2e33c8d9165bcdff44106d3b
Junior Leon <juniorleon@Administrators-MacBook-Pro.local>
2023-02-08 15:48:00 -0500

    final work

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,12 +1,71 @@
-const getNotes = require("./notes.js");
-const msg = getNotes()
-console.log(msg)
-
-const validator = require('validator')
-console.log(validator.isEmail('leon71314@gmail.com'))
-console.log(validator.isEmail('gmail.com'))
-console.log(validator.isURL('https://mead.io'))
-console.log(validator.isURL('mead'))
-
 const chalk = require('chalk')
-console.log(chalk.green.bgRed.bold.inverse("Success!"))
+const yargs = require('yargs')
+const notes = require('./notes.js')
+
+// customize yargs version
+yargs.version('1.1.0')
+
+// add, remove, read, list
+// create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.addNote(argv.title, argv.body)
+    }
+})
+
+// create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.removeNote(argv.title)
+    }
+})
+
+// create list command
+yargs.command({
+    command: 'list',
+    describe: 'list your notes',
+    handler() {
+        notes.listNotes()
+    }
+})
+
+// create read command
+yargs.command({
+    command: 'read',
+    describe: 'read a note',
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler(argv) {
+        notes.readNote(argv.title)
+    }
+})
+
+yargs.parse()
--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,84 @@
-const getNotes = function() {
-    return `Your notes...`   
+const fs = require('fs')
+const chalk = require('chalk')
+
+// add note + duplicates
+const addNote = (title, body) => {
+    const notes = loadNotes()
+    const duplicateNote = notes.find((note) => note.title === title)
+
+    if ( !duplicateNote ) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.bgGreen('New note added!'))
+    } else {
+        console.log(chalk.bgRed('Note title taken!'))
+    }
+}
+// end of the add notes command
+
+//start remove command
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const notesToKeep = notes.filter( (note) => note.title !== title )
+
+    if (notes.length > notesToKeep.length) {
+        console.log(chalk.bgGreen('Note removed!'))
+        saveNotes(notesToKeep)
+    } else {
+        console.log(chalk.bgRed('No note found!'))
+    }
 }
 
-module.exports = getNotes
+// start list command
+const listNotes = () => {
+    const notes = loadNotes()
+
+    console.log(chalk.inverse('Your notes'))
+
+    notes.forEach((note) => {
+        console.log(note.title)
+    })
+}
+
+// start read command
+const readNote = (title) => {
+    const notes = loadNotes()
+    const note = notes.find( (note) => note.title === title )
+
+    if (note) {
+        console.log(chalk.inverse(note.title))
+        console.log(note.body)
+    } else {
+        console.log(chalk.bgRed('Note not found!'))
+    }
+}
+
+// end of all 4 commands
+
+
+// functions needed throughout each command
+const saveNotes= (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (e) {
+        return []
+    }
+}
+
+// exporting each function - basically other files can pull the functions from this page, or what is exported from this page
+module.exports = {
+    addNote: addNote,
+    removeNote: removeNote,
+    listNotes: listNotes,
+    readNote: readNote
+}
